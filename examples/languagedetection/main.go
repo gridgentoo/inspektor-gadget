5fc5bda5d705c24f9545ddc5fa21477f6146dd6a
Mauricio Vásquez <mauriciov@microsoft.com>
2022-11-24 17:22:38 -0500

    Add example to detect programming language of created processes
    POC to print information about the programming languages of the
    processes executed in a cluster.
    
    Output example:
    ```
    $ sudo ./languagedetection
    Collecting info. Press Ctrl + C to print report...^Cstats:
    container            language   executions
    k8s_gadget_gadget-h8tl2_gadget_6d4b2c1c-2792-4e06-b8ec-0cd790c31702_0 go                  4
    k8s_calico-node_calico-node-4mx6g_kube-system_fed47487-2ef6-413c-b854-11709f1fddda_202 go                  1
    k8s_calico-node_calico-node-4mx6g_kube-system_fed47487-2ef6-413c-b854-11709f1fddda_202 c                   1
    k8s_calico-kube-controllers_calico-kube-controllers-56b8f699d9-4nzhg_kube-system_b1035956-1952-44fb-8065-b701e8d69f35_206 go                  2
    ```

---
+++ examples/languagedetection/main.go
@@ -0,0 +1,140 @@
+// Copyright 2022 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/cilium/ebpf/rlimit"
+
+	containercollection "github.com/inspektor-gadget/inspektor-gadget/pkg/container-collection"
+	containerutils "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils"
+	runtimeclient "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/runtime-client"
+	langaugesTracer "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/profile/languages/tracer"
+	tracercollection "github.com/inspektor-gadget/inspektor-gadget/pkg/tracer-collection"
+)
+
+const traceName = "trace_exec"
+
+func main() {
+	var containerName string
+	//	flag.StringVar(&containerName, "containername", "", "Show only data from containers with that name")
+	//	flag.Parse()
+	//
+	//	if containerName == "" {
+	//		fmt.Printf("you must provide --containername\n")
+	//		return
+	//	}
+
+	// In some kernel versions it's needed to bump the rlimits to
+	// use run BPF programs.
+	if err := rlimit.RemoveMemlock(); err != nil {
+		return
+	}
+
+	// Create and initialize the container collection
+	containerCollection := &containercollection.ContainerCollection{}
+
+	tracerCollection, err := tracercollection.NewTracerCollection(containerCollection)
+	if err != nil {
+		fmt.Printf("failed to create trace-collection: %s\n", err)
+		return
+	}
+	defer tracerCollection.Close()
+
+	// Define the different options for the container collection instance
+	opts := []containercollection.ContainerCollectionOption{
+		// Indicate the callback that will be invoked each time
+		// there is an event
+		containercollection.WithPubSub(tracerCollection.TracerMapsUpdater()),
+
+		// Get containers created with runc
+		containercollection.WithRuncFanotify(),
+
+		// Enrich events with Linux namespaces information
+		// It's needed to be able to filter by containers in this example.
+		containercollection.WithLinuxNamespaceEnrichment(),
+
+		// Enrich those containers with data from the container
+		// runtime. docker and containerd in this case.
+		containercollection.WithMultipleContainerRuntimesEnrichment(
+			[]*containerutils.RuntimeConfig{
+				{Name: runtimeclient.DockerName},
+				{Name: runtimeclient.ContainerdName},
+			}),
+	}
+
+	if err := containerCollection.Initialize(opts...); err != nil {
+		fmt.Printf("failed to initialize container collection: %s\n", err)
+		return
+	}
+	defer containerCollection.Close()
+
+	// Create a tracer instance. This is the glue piece that allows
+	// this example to filter events by containers.
+	containerSelector := containercollection.ContainerSelector{
+		Name: containerName,
+	}
+
+	if err := tracerCollection.AddTracer(traceName, containerSelector); err != nil {
+		fmt.Printf("error adding tracer: %s\n", err)
+		return
+	}
+	defer tracerCollection.RemoveTracer(traceName)
+
+	// Get mount namespace map to filter by containers
+	mountnsmap, err := tracerCollection.TracerMountNsMap(traceName)
+	if err != nil {
+		fmt.Printf("failed to get mountnsmap: %s\n", err)
+		return
+	}
+
+	// Create the tracer
+	config := &langaugesTracer.Config{MountnsMap: mountnsmap}
+	tracer, err := langaugesTracer.NewTracer(config, containerCollection)
+	if err != nil {
+		fmt.Printf("error creating tracer: %s\n", err)
+		return
+	}
+
+	fmt.Print("hit ctrcl to finish...")
+
+	// Graceful shutdown
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, syscall.SIGINT, syscall.SIGTERM)
+	<-exit
+
+	ret, err := tracer.Stop()
+	if err != nil {
+		fmt.Printf("error stopping tracer: %s\n", err)
+		return
+	}
+
+	fmt.Printf("\nnamespace, pod, container, language, exections\n")
+
+	for key, val := range ret.Languages {
+		fmt.Printf("%s %s %s %s %d\n",
+			//key.Node,
+			key.Namespace,
+			key.Pod,
+			key.Container,
+			key.Name,
+			*val.Executions,
+		)
+	}
+}
--- go.mod
+++ go.mod
@@ -127,6 +127,7 @@ require (
 	github.com/stretchr/objx v0.4.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/xlab/treeprint v1.1.0 // indirect
+	github.com/xyproto/ainur v1.3.0 // indirect
 	go.starlark.net v0.0.0-20200306205701-8dd3e2ee1dd5 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
