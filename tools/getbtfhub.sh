242de08cbe77b36d804f96d484334ac895534071
Mauricio Vásquez <mauriciov@microsoft.com>
2022-12-22 15:49:41 -0500

    CI: Cache BTFGen
    BTFGen takes a lot of time to execute because it has to:
    1. clone the btfhub-archive repo (some GBs)
    2. uncompress all files from btfhub-archive
    3. run btfgen for all btf files uncompressed above
    
    It's only required to run this command when the bpf objects are changed
    or new BTF files are pushed to btfhub-archive.
    
    This commit modifies the building system to use the Github actions to
    cache the generated BTF files. The changes done here are:
    1. Execute btfgen outside the building container: It's hard to extract
      the BTF files from the container to save them in the cache.
    2. Define a cache for the btf files: It uses as key the architecture,
      the latest commit of btfhub-archive and the hash of all bpf objects.

--- tools/getbtfhub.sh
+++ tools/getbtfhub.sh
@@ -2,5 +2,13 @@
 
 set -e
 
-git clone --depth 1 https://github.com/inspektor-gadget/btfhub /tmp/btfhub -b mauricio/update-bpftool
-git clone --depth 1 https://github.com/aquasecurity/btfhub-archive/ /tmp/btfhub-archive/
+# Don't clone the repo to /tmp because on some systems it's mounted as tempfs and will require a lot
+# of RAM.
+
+if [ ! -d $HOME/btfhub ]; then
+	git clone --depth 1 https://github.com/inspektor-gadget/btfhub $HOME/btfhub -b mauricio/update-bpftool
+fi
+
+if [ ! -d $HOME/btfhub-archive/ ]; then
+	git clone --depth 1 https://github.com/aquasecurity/btfhub-archive/ $HOME/btfhub-archive/
+fi
