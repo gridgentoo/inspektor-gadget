1d933a6c0859b29eafc297c8707832a16b3ab16e
Francis Laniel <flaniel@linux.microsoft.com>
2022-12-28 15:06:04 +0100

    cmd: snapshot: Add functions to print processes like pstree.
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

--- cmd/common/snapshot/process.go
+++ cmd/common/snapshot/process.go
@@ -15,6 +15,9 @@
 package snapshot
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/spf13/cobra"
 
 	commonutils "github.com/inspektor-gadget/inspektor-gadget/cmd/common/utils"
@@ -33,6 +36,85 @@ type ProcessParser struct {
 
 	flags        *ProcessFlags
 	outputConfig *commonutils.OutputConfig
+}
+
+type nodeEvent struct {
+	event    *types.Event
+	children []*nodeEvent
+	parent   *nodeEvent
+}
+
+func createTree(processes []*types.Event) *nodeEvent {
+	nodes := make(map[int]*nodeEvent, 0)
+	// Create a node for each processes.
+	for _, process := range processes {
+		nodes[process.Pid] = &nodeEvent{
+			event:    process,
+			children: make([]*nodeEvent, 0),
+			parent:   nil,
+		}
+	}
+
+	// Link all nodes together.
+	for _, node := range nodes {
+		for _, n := range nodes {
+			if node.event.Pid == n.event.ParentPid {
+				node.children = append(node.children, n)
+				n.parent = node
+			}
+		}
+	}
+
+	// Find the root, i.e. find the node without parent.
+	for _, node := range nodes {
+		if node.parent == nil {
+			return node
+		}
+	}
+
+	return nil
+}
+
+func printNode(node *nodeEvent, front string) {
+	line := fmt.Sprintf("|-%s(%d)", node.event.Command, node.event.Pid)
+	printedLine := front + line
+	if len(node.children) == 0 {
+		fmt.Println(printedLine)
+
+		return
+	}
+
+	printedLine += "-+"
+	// We add one here so the '+' and '|' of the children are aligned.
+	front += strings.Repeat(" ", len(line)+1)
+	fmt.Println(printedLine)
+
+	for _, child := range node.children {
+		printNode(child, front)
+	}
+}
+
+func printTree(tree *nodeEvent) {
+	fmt.Printf("%s (%s)\n", tree.event.Container, tree.event.Namespace)
+	printNode(tree, "")
+}
+
+func PrintTree(processes []*types.Event) error {
+	containers := make(map[string][]*types.Event, 0)
+	for _, process := range processes {
+		_, ok := containers[process.Container]
+		if !ok {
+			containers[process.Container] = make([]*types.Event, 0)
+		}
+		containers[process.Container] = append(containers[process.Container], process)
+	}
+
+	for _, container := range containers {
+		tree := createTree(container)
+		printTree(tree)
+	}
+
+	return nil
 }
 
 func newProcessParser(outputConfig *commonutils.OutputConfig, flags *ProcessFlags, cols *columns.Columns[types.Event], options ...commonutils.Option) (SnapshotParser[types.Event], error) {
